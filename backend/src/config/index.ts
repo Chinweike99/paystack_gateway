16945a945c8de7ea3add0432bede4c8e07703895
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-21 20:58:01 +0100

    fix: run dev dir

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,1 +1,16 @@
-import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+export default {
+    port: process.env.PORT || 5000,
+    paystack: {
+        secretKey: process.env.PAYSTACK_SECRET_KEY,
+        baseURL: process.env.PAYSTACK_BASE_URL
+    },
+    rateLimit: {
+        windowMs: process.env.RATE_LIMIT_WINDOW_MS
+        ? parseInt(process.env.RATE_LIMIT_WINDOW_MS)
+        : 15 * 60 * 1000,
+    max: process.env.RATE_LIMIT_MAX ? parseInt(process.env.RATE_LIMIT_MAX) : 100
+    }
+}
